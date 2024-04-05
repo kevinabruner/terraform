6a42ab7317d27a8156b75be072187245cd97037b
Kevin Bruner-Prime <kevin.a.bruner@gmail.com>
2024-04-05 15:19:57 -0400

    adding a pool

--- terraform-blog/main.tf
+++ terraform-blog/main.tf
@@ -34,7 +34,7 @@ resource "proxmox_lxc" "lxc-test" {
     }
     ostemplate = "shared:vztmpl/ubuntu-22.04-standard_22.04-1_amd64.tar.zst"
     password = "rootroot"
-    pool = "terraform"
+    pool = "HA-3"
     target_node = "pve"
     unprivileged = true
 }
