7fce8e540007c1a18e92fe21ab386acd13bb8e37
Kevin Bruner-Prime <kevin.a.bruner@gmail.com>
2024-04-05 15:30:33 -0400

    cleaning up

--- terraform-blog/main.tf
+++ terraform-blog/main.tf
@@ -7,17 +7,10 @@ terraform {
   }
 }
 
-provider "proxmox" {
-  # url is the hostname (FQDN if you have one) for the proxmox host you'd like to connect to to issue the commands. my proxmox host is 'prox-1u'. Add /api2/json at the end for the API
-  pm_api_url = "https://pve.thejfk.ca/api2/json"
-  
-  # api token id is in the form of: <username>@pam!<tokenId>
-  pm_api_token_id = "terraform@pam!main_terraform"
-  
-  # this is the full secret wrapped in quotes. don't worry, I've already deleted this from my proxmox cluster by the time you read this post
-  pm_api_token_secret = "b01b0155-c025-4b3a-b173-cb6b1bf9eb17"
-  
-  # leave tls_insecure set to true unless you have your proxmox SSL certificate situation fully sorted out (if you do, you will know)
+provider "proxmox" {  
+  pm_api_url = "https://pve.thejfk.ca/api2/json"    
+  pm_api_token_id = "terraform@pam!main_terraform"    
+  pm_api_token_secret = "b01b0155-c025-4b3a-b173-cb6b1bf9eb17"    
   pm_tls_insecure = false
 }
 
