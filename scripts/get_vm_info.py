0b9bc8ed5e52ab4fa2899e6a8fa93cde6c9302dd
Kevin Bruner-Prime <kevin.a.bruner@gmail.com>
2024-04-05 16:38:21 -0400

    remove remove

--- scripts/get_vm_info.py
+++ scripts/get_vm_info.py
@@ -39,8 +39,7 @@ all_vms = {"vm_results": []}
 for result in data["results"]:    
     if result["primary_ip4"]:
         curDir = gitDir + '/vms/' + result["name"]
-        
-        shutil.rmtree(curDir)            
+                
         os.makedirs(curDir)        
 
         shutil.copy(gitDir + '/main.template', curDir)
