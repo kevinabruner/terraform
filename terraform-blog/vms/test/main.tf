4e102d3135ad103406119cafb6975da9aaf19238
Kevin Bruner-Prime <kevin.a.bruner@gmail.com>
2024-04-05 16:04:15 -0400

    putting the provider back

--- terraform-blog/vms/test/main.tf
+++ terraform-blog/vms/test/main.tf
@@ -1,3 +1,19 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source = "telmate/proxmox"
+      version = "2.9.14"
+    }
+  }
+}
+
+provider "proxmox" {  
+  pm_api_url = "https://pve.thejfk.ca/api2/json"    
+  pm_api_token_id = "terraform@pam!main_terraform"    
+  pm_api_token_secret = "b01b0155-c025-4b3a-b173-cb6b1bf9eb17"    
+  pm_tls_insecure = false
+}
+
 resource "proxmox_lxc" "vm_name" {
     count = 1
     features {
