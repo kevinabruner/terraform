8cda8dc6a4348710d492c7047d475af29bb5361d
Kevin Bruner-Prime <kevin.a.bruner@gmail.com>
2024-04-07 11:34:36 -0400

    unsaved

--- scripts/get_vm_info.py
+++ scripts/get_vm_info.py
@@ -95,8 +95,7 @@ for result in data["results"]:
             shutil.copy(gitDir + '/main.template', curDir + '/main.tf')
             shutil.copy(os.path.join(gitDir, 'vars.template'), os.path.join(curDir, 'vars.tf'))
             shutil.copy(gitDir + '/cloud-init.cloud_config.template', curDir + '/files/cloud-init.cloud_config.tftpl')
-            
-            
+                        
                         
             moduleLine = "module \"" + result["name"] + "\" { source = \"/home/kevin/terraform/vms/" + result["name"] + "\" }"
             with open(gitDir + '/main.tf', 'a') as file:
