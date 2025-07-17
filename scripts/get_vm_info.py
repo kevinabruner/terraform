cac5d8293d5f0bff5f3efb024ec8c4993aaabd8f
Kevin Bruner-Prime <kevin.a.bruner@gmail.com>
2025-07-17 18:29:16 -0400

    more vars

--- scripts/get_vm_info.py
+++ scripts/get_vm_info.py
@@ -123,7 +123,10 @@ for vm in vms["results"]:
                     bridge = "vmbr3"
                 }"""
                 
-                replace_text_in_file(curDir + "/main.tf" , "@@@fastip", "ipconfig1 = \"" + vm["custom_fields"]["FAST_IP"]["address"].split('/')[0] + "\"")   
+                ip_addr = vm["custom_fields"]["FAST_IP"]["address"].split('/')[0]
+                fast_ip_text = f'ipconfig1 = "ip={ip_addr}"'
+                replace_text_in_file(curDir + "/main.tf", "@@@fastip", fast_ip_text)
+
             else:                
                 replace_text_in_file(curDir + "/main.tf" , "@@@fastip", "")       
 
