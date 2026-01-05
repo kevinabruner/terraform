8d2c1f7ce54bab8955f5d950b01f3ad50f5ef288
Kevin Bruner-Prime <kevin.a.bruner@gmail.com>
2026-01-05 16:39:40 -0500

    Netbox (#6)
    * fucking yolo
    
    * back to main.tf baby
    
    * diff source
    
    * pve
    
    * adding locals
    
    * json
    
    * robust locals
    
    * clank
    
    * token
    
    * already string?
    
    * already string?
    
    * hoo boy
    
    * ssh keys
    
    * s
    
    * updating template
    
    * now the fun begins
    
    * hoo boy
    
    * single line
    
    * ignore changes
    
    * ssssh
    
    * password
    
    * format
    
    * adding netbox template
    
    * now with some vars
    
    * sean paul
    
    * again
    
    * vm username
    
    * whitespace
    
    * adding vars
    
    * quotes
    
    * netbox token secret
    
    * syntax
    
    * notes
    
    * no more local ssh keys
    
    * comma
    
    * setting contrext
    
    * ugh
    
    * adding readme
    
    * no need for a script
    
    * notes
    
    * more notes

--- main.tf
+++ main.tf
@@ -4,15 +4,130 @@ terraform {
       source = "telmate/proxmox"
       version = "3.0.2-rc07"
     }
+    netbox = {
+      source  = "e-breuninger/netbox"
+      version = "~> 5.0" # Or current version
+    }
   }
 }
 
+data "http" "netbox_export" {
+  url = "https://netbox.thejfk.ca/api/virtualization/virtual-machines/?export=Main+terraform+templates"
+
+  request_headers = {
+    Authorization = "Token ${var.netbox_api_token_secret}"
+    Accept = "application/json"
+  }
+}
+
+
 provider "proxmox" {  
-  pm_api_url = "https://192.168.11.15/api2/json"    
+  pm_api_url = "https://pve.thejfk.ca/api2/json"    
   pm_api_token_id = "terraform@pam!main_terraform"    
-  pm_api_token_secret = "b01b0155-c025-4b3a-b173-cb6b1bf9eb17"    
+  pm_api_token_secret = var.proxmox_api_token_secret
   pm_tls_insecure = false
 }
 
 
-###generated###
+locals {
+  # This will now work because NetBox is sending valid JSON
+  vms = jsondecode(data.http.netbox_export.response_body)
+}
+
+
+resource "proxmox_vm_qemu" "proxmox_vms" {
+  for_each = { for vm in local.vms : vm.name => vm if vm.name != "" }
+
+  name        = each.value.name
+  vmid        = each.value.vmid
+  target_node = each.value.node
+  description = each.value.desc
+  pool        = each.value.pool != "" ? each.value.pool : null
+  
+  agent    = 1
+  memory   = each.value.memory
+  os_type  = "cloud-init"
+  scsihw   = "virtio-scsi-pci"
+  boot     = "order=scsi0;ide3"
+
+  cpu {
+    cores   = each.value.cores
+    sockets = 1
+    type    = "host"
+  }
+
+  disks {
+    scsi {
+      scsi0 {
+        disk {
+          storage   = each.value.storage
+          size      = each.value.disk_size
+          replicate = true
+          format    = "raw"
+        }
+      }
+    }
+    ide {      
+      ide2 { 
+        cdrom { 
+          passthrough = false 
+        } 
+      }      
+      ide3 { 
+        cloudinit { 
+          storage = each.value.storage 
+        } 
+      }
+    }
+  }
+
+  serial { 
+    id   = 0 
+    type = "socket" 
+  }
+
+  # Networking
+  network {
+    id     = 0
+    model  = "virtio"
+    bridge = "vmbr0"
+    tag    = each.value.vlan != 0 ? each.value.vlan : null
+  }
+
+  ipconfig0 = each.value.ip0
+
+  # Fast IP Logic
+  dynamic "network" {
+    for_each = each.value.fast_ip != "" ? [1] : []
+    content {
+      id     = 1
+      model  = "virtio"
+      bridge = "vmbr3"
+    }
+  }
+  ipconfig1 = each.value.fast_ip != "" ? "ip=${each.value.fast_ip}" : null
+
+  # Standardized user data
+  ciuser     = var.vm_username
+  cipassword = var.vm_password
+  sshkeys = each.value.ssh_keys
+
+  lifecycle {
+    ignore_changes = [
+      qemu_os, 
+      start_at_node_boot, 
+      hagroup, 
+      hastate,      
+      vm_state, 
+      agent, 
+      usbs, 
+      tags, 
+      network, 
+      startup_shutdown,
+      clone, 
+      full_clone, 
+      ipconfig1
+    ]
+  }
+}
+
