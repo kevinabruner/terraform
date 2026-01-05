41bcf538c166751c1eed7e30af45efc3a5408547
Kevin Bruner-Prime <kevin.a.bruner@gmail.com>
2026-01-05 16:00:01 -0500

    quotes

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ data "http" "netbox_export" {
 provider "proxmox" {  
   pm_api_url = "https://pve.thejfk.ca/api2/json"    
   pm_api_token_id = "terraform@pam!main_terraform"    
-  pm_api_token_secret = "var.proxmox_api_token_secret"    
+  pm_api_token_secret = var.proxmox_api_token_secret
   pm_tls_insecure = false
 }
 
