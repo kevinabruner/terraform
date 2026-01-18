b184294fb5d1e6e00fbb498ca10ee129d2b75d30
Kevin Bruner-Prime <kevin.a.bruner@gmail.com>
2026-01-17 22:07:48 -0500

    clank

--- main.tf
+++ main.tf
@@ -1,49 +1,38 @@
 terraform {
   required_providers {
     proxmox = {
-      source = "telmate/proxmox"
+      source  = "telmate/proxmox"
       version = "3.0.2-rc07"
     }
     netbox = {
       source  = "e-breuninger/netbox"
-      version = "~> 5.0" 
+      version = "~> 5.0"
     }
   }
 }
 
 data "http" "netbox_export" {
   url = "https://netbox.thejfk.ca/api/virtualization/virtual-machines/?export=Main+terraform+templates"
-
   request_headers = {
     Authorization = "Token ${var.netbox_api_token_secret}"
-    Accept = "application/json"
+    Accept        = "application/json"
   }
 }
 
-
-provider "proxmox" {  
-  pm_api_url = "${var.proxmox_api_url}"    
-  pm_api_token_id = "${var.proxmox_api_token_id}"    
-  pm_api_token_secret = "${var.proxmox_api_token_secret}"
-  pm_tls_insecure = false
-  pm_parallel = 3
+provider "proxmox" {
+  pm_api_url          = var.proxmox_api_url
+  pm_api_token_id     = var.proxmox_api_token_id
+  pm_api_token_secret = var.proxmox_api_token_secret
+  pm_tls_insecure     = false
+  pm_parallel         = 3
 }
-
 
 locals {
   vms = jsondecode(data.http.netbox_export.response_body)
   vm_configs = {
     for vm in local.vms : vm.name => merge(vm, {
-
-      # Find the specific interface object where is_primary is true
       primary_iface = [for i in vm.interfaces : i if i.is_primary][0]
-
-      # Find all interfaces that are NOT primary
-      secondary_ifaces = [for i in vm.interfaces : i if !i.is_primary]
-      
-      # Use that found object generate a gateway address (192.168.xx + .1)
-      gateway = "${join(".", slice(split(".", [for i in vm.interfaces : i.ip if i.is_primary][0]), 0, 3))}.1"
-
+      gateway       = "${join(".", slice(split(".", [for i in vm.interfaces : i.ip if i.is_primary][0]), 0, 3))}.1"
     }) if vm.name != ""
   }
 }
@@ -55,55 +44,54 @@ resource "proxmox_cloud_init_disk" "ci_configs" {
   storage  = "cephfs"
 
   meta_data = <<-EOT
-instance-id: ${each.value.name}
-local-hostname: ${each.value.name}
-EOT
+    instance-id: ${each.value.name}
+    local-hostname: ${each.value.name}
+  EOT
 
   user_data = <<-EOT
-#cloud-config
-users:
-  - name: ${var.vm_username}
-    passwd: '${var.vm_password}'  
-    lock_passwd: false
-    sudo: ALL=(ALL) NOPASSWD:ALL
-    groups: [adm, conf, dip, lxd, plugdev, sudo]
-    shell: /bin/bash
-    ssh_authorized_keys:
-%{ for key in split("\n", trimspace(each.value.ssh_keys)) ~}
-      - ${trimspace(key)}
-%{ endfor ~}
+    #cloud-config
+    users:
+      - name: ${var.vm_username}
+        passwd: '${var.vm_password}'
+        lock_passwd: false
+        sudo: ALL=(ALL) NOPASSWD:ALL
+        groups: [adm, conf, dip, lxd, plugdev, sudo]
+        shell: /bin/bash
+        ssh_authorized_keys:
+    %{ for key in split("\n", trimspace(each.value.ssh_keys)) ~}
+          - ${trimspace(key)}
+    %{ endfor ~}
 
-runcmd:
-  - systemctl enable qemu-guest-agent
-  - systemctl start qemu-guest-agent
+    runcmd:
+      - systemctl enable qemu-guest-agent
+      - systemctl start qemu-guest-agent
 
-write_files:
-  - path: /etc/environment
-    content: |
-      NETBOX_ID=${each.value.vmid}
-      VM_NAME=${each.value.name}
-    append: true
-EOT
+    write_files:
+      - path: /etc/environment
+        content: |
+          NETBOX_ID=${each.value.vmid}
+          VM_NAME=${each.value.name}
+        append: true
+  EOT
 
   network_config = <<-EOT
-version: 2
-ethernets:
-%{ for index, iface in each.value.interfaces ~}
-  ens${18 + index}:
-    addresses:
-      - ${iface.ip}
-    %{ if iface.is_primary ~}
-    gateway4: ${each.value.gateway}
-    nameservers:
-      addresses: [192.168.11.99]
-    %{ endif ~}
-%{ endfor ~}
-EOT
+    version: 2
+    ethernets:
+    %{ for index, iface in each.value.interfaces ~}
+      ens${18 + index}:
+        addresses:
+          - ${iface.ip}
+        %{ if iface.is_primary ~}
+        gateway4: ${each.value.gateway}
+        nameservers:
+          addresses: [192.168.11.99]
+        %{ endif ~}
+    %{ endfor ~}
+  EOT
 }
 
 resource "proxmox_vm_qemu" "proxmox_vms" {
-  for_each = local.vm_configs
-
+  for_each           = local.vm_configs
   name               = each.value.name
   vmid               = each.value.vmid
   target_node        = each.value.node
@@ -112,7 +100,7 @@ resource "proxmox_vm_qemu" "proxmox_vms" {
   start_at_node_boot = each.value.start_at_node_boot
   agent              = 1
   memory             = each.value.memory
-  clone              = each.value.image   # This comes from your NetBox "image" field
+  clone              = each.value.image
   full_clone         = true
   os_type            = "ubuntu"
   scsihw             = "virtio-scsi-pci"
@@ -130,7 +118,7 @@ resource "proxmox_vm_qemu" "proxmox_vms" {
     content {
       id     = network.key
       model  = "virtio"
-      bridge = network.value.name 
+      bridge = network.value.name
       tag    = network.value.vlan > 0 ? network.value.vlan : null
     }
   }
@@ -146,13 +134,13 @@ resource "proxmox_vm_qemu" "proxmox_vms" {
         }
       }
     }
-    ide {      
-      ide2 { 
-        cdrom { 
-          passthrough = false 
-        } 
-      }      
-      ide3 { 
+    ide {
+      ide2 {
+        cdrom {
+          passthrough = false
+        }
+      }
+      ide3 {
         cdrom {
           iso = proxmox_cloud_init_disk.ci_configs[each.key].id
         }
@@ -160,26 +148,22 @@ resource "proxmox_vm_qemu" "proxmox_vms" {
     }
   }
 
-  serial { 
-    id   = 0 
-    type = "socket" 
+  serial {
+    id   = 0
+    type = "socket"
   }
 
-
- 
-
-lifecycle {
+  lifecycle {
     ignore_changes = [
-      qemu_os, 
-      hagroup, 
-      hastate,            
-      agent, 
-      usbs, 
-      tags, 
+      qemu_os,
+      hagroup,
+      hastate,
+      agent,
+      usbs,
+      tags,
       startup_shutdown,
       clone,
-      full_clone,  
+      full_clone,
     ]
-  } 
+  }
 }
-
