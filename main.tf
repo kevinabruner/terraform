96a680a17c6117cbf649935f08180ec15e47913c
Kevin Bruner-Prime <kevin.a.bruner@gmail.com>
2026-01-19 00:51:01 -0500

    indent

--- main.tf
+++ main.tf
@@ -110,7 +110,7 @@ ethernets:
     gateway4: ${each.value.gateway}
     nameservers:
       addresses: [192.168.11.99]
-    search: [jfkhome]
+      search: [jfkhome]
     routes:
       - to: default
         via: ${each.value.gateway}
