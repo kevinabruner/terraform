70a51d7c326b874cdd6a3b2f98bb6c6e101e2207
Kevin Bruner-Prime <kevin.a.bruner@gmail.com>
2026-02-08 16:20:27 -0500

    parallel is back

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ provider "proxmox" {
   pm_api_token_id     = var.proxmox_api_token_id
   pm_api_token_secret = var.proxmox_api_token_secret
   pm_tls_insecure     = false
-  #pm_parallel         = 3
+  pm_parallel         = 3
 }
 
 
