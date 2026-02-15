3b7724ca1dd9e4b77e45a8b848fbc772184f1c7c
Kevin Bruner-Prime <kevin.a.bruner@gmail.com>
2026-02-15 14:34:15 -0500

    removing debug

--- main.tf
+++ main.tf
@@ -203,30 +203,30 @@ resource "proxmox_vm_qemu" "proxmox_vms" {
     ]
   }
 }
-resource "local_file" "debug_rendered_yaml" {
-  for_each = local.vm_configs
-  content  = proxmox_cloud_init_disk.ci_configs[each.key].user_data
-  filename = "${path.module}/debug/${each.key}_cloud_init.yaml"
-}
-resource "local_file" "debug_network_config" {
-  for_each = local.vm_configs
-  content  = <<-EOT
-version: 2
-ethernets:
-%{ for index, iface in each.value.interfaces ~}
-  ens${18 + index}:
-    addresses:
-      - ${iface.ip}
-%{ if iface.is_primary ~}
-    gateway4: ${each.value.gateway}
-    nameservers:
-      addresses: [192.168.11.99]
-      search: [jfkhome]
-    routes:
-      - to: default
-        via: ${each.value.gateway}
-%{ endif ~}
-%{ endfor ~}
-EOT
-  filename = "${path.module}/debug/debug_${each.key}.yaml"
-}
+# resource "local_file" "debug_rendered_yaml" {
+#   for_each = local.vm_configs
+#   content  = proxmox_cloud_init_disk.ci_configs[each.key].user_data
+#   filename = "${path.module}/debug/${each.key}_cloud_init.yaml"
+# }
+# resource "local_file" "debug_network_config" {
+#   for_each = local.vm_configs
+#   content  = <<-EOT
+# version: 2
+# ethernets:
+# %{ for index, iface in each.value.interfaces ~}
+#   ens${18 + index}:
+#     addresses:
+#       - ${iface.ip}
+# %{ if iface.is_primary ~}
+#     gateway4: ${each.value.gateway}
+#     nameservers:
+#       addresses: [192.168.11.99]
+#       search: [jfkhome]
+#     routes:
+#       - to: default
+#         via: ${each.value.gateway}
+# %{ endif ~}
+# %{ endfor ~}
+# EOT
+#   filename = "${path.module}/debug/debug_${each.key}.yaml"
+# }
