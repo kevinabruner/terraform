a6a3ed5653ec990cbdfebd9b4b0aa569aa6c8502
Kevin Bruner-Prime <kevin.a.bruner@gmail.com>
2026-02-15 14:30:25 -0500

    revert

--- main.tf
+++ main.tf
@@ -158,7 +158,7 @@ resource "proxmox_vm_qemu" "proxmox_vms" {
     content {
       id     = network.key
       model  = "virtio"
-      bridge = lookup(network.value, "bridge", "vmbr0")
+      bridge = network.value.bridge
       tag    = network.value.vlan > 0 ? network.value.vlan : null
     }
   }
