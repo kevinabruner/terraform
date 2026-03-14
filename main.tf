92b13a8cc761bdd45ccaa96d87a88418d1afa319
Kevin Bruner-Prime <kevin.a.bruner@gmail.com>
2026-03-14 12:43:01 -0400

    sudo

--- main.tf
+++ main.tf
@@ -109,9 +109,9 @@ locals {
       has_keepalived = false
       packages       = ["unattended-upgrades"]
       commands       = [
-        "DEBIAN_FRONTEND=noninteractive apt-get install -y /home/kevin/plexmediaserver.deb",  
+        "sudo DEBIAN_FRONTEND=noninteractive apt-get install -y /home/kevin/plexmediaserver.deb",  
         "/usr/bin/id -u plex >/dev/null 2>&1 && (/usr/bin/pkill -u plex || true; /usr/sbin/userdel -f plex || true)",
-        "apt-get install -f -y",      
+        "sudo apt-get install -f -y",      
         "cp /opt/Tautulli/init-scripts/init.systemd /lib/systemd/system/tautulli.service",
         "sed -i '/^ExecStart=/i ExecStartPre=/bin/sleep 90' /lib/systemd/system/tautulli.service",
         "systemctl daemon-reload",
