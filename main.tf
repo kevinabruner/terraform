88a2900a14652c34e6bf5530b02f30c926157d56
Kevin Bruner-Prime <kevin.a.bruner@gmail.com>
2026-03-14 12:31:16 -0400

    clank

--- main.tf
+++ main.tf
@@ -110,7 +110,7 @@ locals {
       packages       = ["unattended-upgrades"]
       commands       = [
         "DEBIAN_FRONTEND=noninteractive apt-get install -y /home/kevin/plexmediaserver.deb",  
-        "/user/sbin/userdel plex",
+        "/usr/bin/id -u plex >/dev/null 2>&1 && (/usr/bin/pkill -u plex || true; /usr/sbin/userdel -f plex || true)",
         "apt-get install -f -y",      
         "cp /opt/Tautulli/init-scripts/init.systemd /lib/systemd/system/tautulli.service",
         "sed -i '/^ExecStart=/i ExecStartPre=/bin/sleep 90' /lib/systemd/system/tautulli.service",
